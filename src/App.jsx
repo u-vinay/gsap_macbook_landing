58648cbbe7c3a2590d09fb2b38bd49e3c3bc9e3a
u-vinay <vinayu9748@gmail.com>
2025-11-26 19:51:44 +0530

    model rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from "./components/NavBar.jsx";
 import {Hero} from "./components/hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
 
 const App = () => {
   return (
      <main>
          <NavBar />
          <Hero />
+         <ProductViewer />
      </main>
   )
 }
