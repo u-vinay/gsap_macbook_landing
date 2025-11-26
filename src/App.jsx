1a59b59cb1b6a1ef96c4953c87ac388ecbe1e8b7
u-vinay <vinayu9748@gmail.com>
2025-11-26 17:36:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from "./components/NavBar.jsx";
+
+const App = () => {
+  return (
+     <main>
+         <NavBar />
+     </main>
+  )
+}
+
+export default App
